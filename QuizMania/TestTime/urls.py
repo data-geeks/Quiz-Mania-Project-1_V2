155465b20addb78ccbaf06f84adfffdd02bcc508
piyush <piyushagarwal.0108@gmail.com>
2019-09-30 18:33:15 +0530

    added the documentation for all

--- QuizMania/TestTime/urls.py
+++ QuizMania/TestTime/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test_page',views.test,name='test_page'),
-    path('base',views.initial,name='base')
+    path('test_page',views.test,name='test_page'),  # this is link to main test page changes dynamically
+    path('base',views.initial,name='base')  # this is link to index test page stays static
 ]

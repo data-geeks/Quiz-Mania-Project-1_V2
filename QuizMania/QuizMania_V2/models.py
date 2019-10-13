bf02b5e15a8a82e62dc33589f2794028e5b16dcf
piyush <piyushagarwal.0108@gmail.com>
2019-10-13 21:56:54 +0530

    file uploading and saving data in a database is done

--- QuizMania/QuizMania_V2/models.py
+++ QuizMania/QuizMania_V2/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+class Working_Test(models.Model):
+    TestName = models.CharField(max_length=100)
+    TestCategory = models.CharField(max_length=100)
+    Duration = models.CharField(max_length=50)
+    Level = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'''
+        Test Name: {self.TestName}
+        Category : {self.TestCategory} 
+        Duration : {self.Duration} 
+        Level    : {self.Level}
+        '''
--- QuizMania/QuizMania_V2/views.py
+++ QuizMania/QuizMania_V2/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render,reverse
+from django.shortcuts import render,reverse,redirect
 from django.http import Http404,HttpResponseRedirect,HttpResponse  
 from django.contrib.auth import authenticate,login,logout 
 from django.contrib.auth.models import User 
 from TestTime.models import Score
+from .models import Working_Test
+from django.core.files.storage import FileSystemStorage 
+
 # Create your views here.
+
+def redirect_stats(request):
+    if request.user.email == 'adhocnetworks@adhoc.co.in':
+        return redirect('admin_portal')
+    else:
+        return HttpResponseRedirect('login_reg')
+
 def index(request):
     return render(request,"QuizMania_V2/Catalog.html")
 
@@ -27,9 +37,17 @@ def add_test(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login_page'))
     else:
-        course_name = request.POST['cname']
-        course_category = request.POST['category']
-        course_duration = request.POST['duration']
-        course_level = request.POST['level']
-        return render(request,'QuizMania_V2/admin.html')
+        if request.method == 'POST':
+            course_name = request.POST['cname']
+            course_category = request.POST['category']
+            course_duration = request.POST['duration']
+            course_level = request.POST['level']
+            test = Working_Test(TestName=course_name,TestCategory=course_category,Duration=course_duration,Level=course_level)
+            test.save()
+            uploaded_file = request.FILES['test_file']
+            fs = FileSystemStorage()
+            fs.save(uploaded_file.name,uploaded_file)    
+            return HttpResponseRedirect('admin_portal')
+        else:
+            return HttpResponse(request,"Please Contact Pykid...error has arrived")
 

2a89239afbe738c3e2becb9fbc9a60cdc3fef1a6
piyush <piyushagarwal.0108@gmail.com>
2019-10-26 23:39:05 +0530

    entered the documentation

--- QuizMania/media/SetQuestions.py
+++ QuizMania/media/SetQuestions.py
@@ -16,7 +16,10 @@ conn=mysql.connect(user=username,password=password,database=database_name,host=h
 
 # Now generating a SQL language cursor 
 cur = conn.cursor()
-table_name = input('Enter the table name please')
+print("Hey Welcome to Test Adder")
+print("Remember the Test Table name starts with TestTime")
+print("e.g., TestTime_aws_b ( where aws is testname and b defines the level)")
+table_name = input('Enter the table name please: ')
 sql = f"INSERT INTO {table_name}(Question,Option1,Option2,Option3,Option4,Answer) VALUES (%s,%s,%s,%s,%s,%s)"
 
 textfile='''
@@ -28,8 +31,8 @@ with open('file.txt','r') as file:
 '''
 
 filename = input("please the enter the test xml file with extension to enter into database: ")
-tree = ET.ElementTree(file=filename)
-root = tree.getroot()
+tree = ET.ElementTree(file=filename)  # main file
+root = tree.getroot()  # Root node doc
 for elem in root:
     question = elem[0].text
     option1 = elem[1].text
@@ -37,6 +40,7 @@ for elem in root:
     option3 = elem[3].text
     option4 = elem[4].text
     answer = elem[5].text
+    # to save everything in database
     cur.execute(sql,(question,option1,option2,option3,option4,answer))
 
 conn.commit()
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Quiz-Mania-Project-1_V2
 this is the DJango version of the QuizMania project build in CGI
+
+## Steps to Add a new TestPage On Website
+1. The file should be made in ```xml``` format given in ```media``` folder
+2. As long as The test box can be seen on website
+    1. Save the test page file with same name as test with its level ```docker_b.xml```
+    2. Run ```python3 SetQuestions.py```
+    3. When prompted enter the table name ```TestTime_docker_b```
+    4. When prompted enter the test file name ```docker_b.xml```
+    5. Enter and test information will be added to database
+3. Reload the server and system settings by
+    ```shell
+    $sudo systemctl daemon-reload
+    $sudo systemctl restart gunicorn
+    $sudo service restart nginx
+    ```
